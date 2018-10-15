ee37bc953f66b05829d3aa2ade42afc74ba1cabb
Cristian Osorio <cristian.osorio@globant.com>
2018-10-15 19:01:35 -0300

    Add code http

--- src/main/java/com/chokkoazul/pocspringbootmentoring/controller/ProductController.java
+++ src/main/java/com/chokkoazul/pocspringbootmentoring/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.chokkoazul.pocspringbootmentoring.model.Product;
 import com.chokkoazul.pocspringbootmentoring.repository.ProductRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,8 +22,16 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        HttpStatus status = HttpStatus.OK;
+        List<Product> list = null;
+        try {
+            list = productRepository.findAll();
+        } catch (Exception e) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<>(list, status);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -36,18 +46,22 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Product createProduct(@Valid @RequestBody Product product) {
-        product.set_id(ObjectId.get());
-        productRepository.save(product);
-        return product;
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        HttpStatus status = HttpStatus.CREATED;
+        try {
+            product.set_id(ObjectId.get());
+            productRepository.save(product);
+        } catch (Exception e) {
+            product = null;
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(product, status);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteProduct(@PathVariable ObjectId id) {
         productRepository.delete(productRepository.findBy_id(id));
     }
-
-
 
 
 }
